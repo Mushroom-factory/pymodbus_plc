9455eb1cccf54d8707eb5a78aeb1ceb1840da7c9
mrzaizai2k <chibao24.12.1999@gmail.com>
2023-03-24 21:15:21 +0700

    read regis 4

--- multi_registers.py
+++ multi_registers.py
@@ -7,25 +7,26 @@ from pymodbus.constants import Endian
 
 plc_ip = '192.168.1.10' # replace with your PLC's IP address
 plc_port = 503 # replace with your PLC's Modbus TCP/IP port number
-start_address = 101
+start_address = 0
 # Can't be 30101 because it's higher than the register
 # should be 101 or 0x01
 
 # Set the number of registers to read
-num_registers = 20
+num_registers = 10 # Must be the same as in PLC
 
 client = ModbusTcpClient(plc_ip, port=plc_port)
 client.connect()
 
-result = client.read_input_registers(address= start_address, count=num_registers, unit=2)
+result = client.read_holding_registers(address= start_address, count=num_registers, unit=2)
+
 
 if result.isError():
     print('Error reading registers:', result)
 else:
     decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big)
-    value1 = decoder.decode_32bit_float()
-    value2 = decoder.decode_32bit_float()
-    value3 = decoder.decode_32bit_float()
+    value1 = decoder.decode_16bit_int()
+    value2 = decoder.decode_16bit_int()
+    value3 = decoder.decode_16bit_int()
     print('value 1 = {}, value 2 = {}, value 3 = {}'.format(value1,value2,value3))
 
 
